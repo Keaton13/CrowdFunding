6c4920c3909e5202c813fbed432dae1c56519dc8
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-07-05 18:21:10 -0700

    Front-End initalized

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -32,7 +32,15 @@ module.exports = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.17",
+    version: "0.8.9",
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: 'https://rpc.ankr.com/eth_sepolia',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
